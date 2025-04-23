a760d920b867aaa5aa13b0584dc96060b67b2405
srikar0000 <thatavarthisrikar@gmail.com>
2025-04-23 23:24:03 +0530

    Add files via upload

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SampleTest {
+
+    @Test
+    public void testSomething() {
+        assertEquals(2, 1 + 1);
+    }
+}
